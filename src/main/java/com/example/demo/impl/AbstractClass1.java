d59d5c043b889ff4785d9735f3201ae8a36a1c3b
Ranomenjanahary <hei.jaonary@gmail.com>
2023-04-17 16:02:09 +0300

    feat: implement AbstractClass1

---
+++ src/main/java/com/example/demo/impl/AbstractClass1.java
@@ -0,0 +1,17 @@
+package com.example.demo.impl;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class AbstractClass1 {
+    @Autowired
+    private ConcreteDependance1 concreteDependance1;
+
+    @Autowired
+    private ConcreteDependance2 concreteDependance2;
+}
